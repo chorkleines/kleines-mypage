e735afd097cb6c37c57a4afc05e30bedacb97813
Nozomu Yamazaki <46639706+nozomu-y@users.noreply.github.com>
2022-09-10 10:42:51 +0900

    Create database migrations for users

---
+++ database/migrations/2022_09_10_012809_create_users_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUsersTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('users', function(Blueprint $table)
+		{
+			$table->increments('user_id');
+			$table->string('email', 256)->nullable()->unique('email');
+			$table->string('password', 256)->nullable();
+			$table->string('status', 32)->nullable();
+		});
+	}
+
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('users');
+	}
+
+}
