6e425098293d8b51e1b708edba512da539216450
Nozomu Yamazaki <46639706+nozomu-y@users.noreply.github.com>
2022-09-10 11:15:26 +0900

    Format

--- database/migrations/2022_09_10_012809_create_users_table.php
+++ database/migrations/2022_09_10_012809_create_users_table.php
@@ -4,33 +4,30 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration {
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->increments('user_id');
+            $table->string('email', 256)->nullable()->unique('email');
+            $table->string('password', 256)->nullable();
+            $table->string('status', 32)->nullable();
+        });
+    }
 
-	/**
-	 * Run the migrations.
-	 *
-	 * @return void
-	 */
-	public function up()
-	{
-		Schema::create('users', function(Blueprint $table)
-		{
-			$table->increments('user_id');
-			$table->string('email', 256)->nullable()->unique('email');
-			$table->string('password', 256)->nullable();
-			$table->string('status', 32)->nullable();
-		});
-	}
-
-
-	/**
-	 * Reverse the migrations.
-	 *
-	 * @return void
-	 */
-	public function down()
-	{
-		Schema::drop('users');
-	}
-
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('users');
+    }
 }
