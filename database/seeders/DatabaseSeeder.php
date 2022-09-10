7d1427834c4be0a70804a0f49a4fabfe4796a363
Nozomu Yamazaki <nozoyama39@gmail.com>
2022-09-10 21:43:40 +0900

    Skip seeding if production

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,6 +17,8 @@ class DatabaseSeeder extends Seeder
     {
         // \App\Models\User::factory(10)->create();
 
+        if (!app()->isProduction()) return;
+
         \App\Models\User::create([
             'email' => 'admin@chorkleines.com',
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password is password
