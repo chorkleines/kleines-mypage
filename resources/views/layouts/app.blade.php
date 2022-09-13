1bfa13536118d0b29625e9d01a84608e06e1ed11
Nozomu Yamazaki <nozoyama39@gmail.com>
2022-09-13 23:06:56 +0900

    Remove margins

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -75,7 +75,7 @@
                 </div>
             </nav>
             <div class="col-md-9 ms-sm-auto col-lg-10 px-md-4 py-4">
-                <main class="py-4">
+                <main>
                     @yield('content')
                 </main>
             </div>
