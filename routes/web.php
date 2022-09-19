13f4d1c43568c072e7c453690b3b684fbbadf584
Nozomu Yamazaki <nozoyama39@gmail.com>
2022-09-19 17:43:53 +0900

    Change home path to /

--- routes/web.php
+++ routes/web.php
@@ -13,12 +13,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+ use App\Http\Controllers\HomeController;
 
 Auth::routes([
     'register' => false,
 ]);
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/', [HomeController::class, 'index'])->name('home');
