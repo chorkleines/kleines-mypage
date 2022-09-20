a842ae76c52d266afe6128f24ef0242309db111e
Nozomu Yamazaki <nozoyama39@gmail.com>
2022-09-20 10:16:46 +0900

    Format

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -54,7 +54,8 @@
                                     <a class="dropdown-item" href="{{ route('logout') }}"
                                         onclick="event.preventDefault();
                                                              document.getElementById('logout-form').submit();">{{ __('logout') }}</a>
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                                    <form id="logout-form" action="{{ route('logout') }}" method="POST"
+                                        class="d-none">
                                         @csrf
                                     </form>
                                 </li>
