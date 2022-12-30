20811a8b14451b658696ce858b0a259d5f446bce
Nozomu Yamazaki <nozoyama39@gmail.com>
2022-12-30 15:54:08 +0900

    Update: create database seeder for random accounting records

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -40,22 +40,10 @@ class DatabaseSeeder extends Seeder
         echo 'Email: admin@chorkleines.com'.PHP_EOL;
         echo 'Password: password'.PHP_EOL;
 
+        # create random users
         \App\Models\User::factory(100)->has(\App\Models\Profile::factory())->create();
 
-        $accounting_list = \App\Models\AccountingList::create([
-            'name' => 'テスト集金',
-            'deadline' => date('2021-12-31'),
-            'admin' => AccountingType::GENERAL,
-        ]);
-
-        \App\Models\AccountingRecord::create([
-            'accounting_id' => $accounting_list->accounting_id,
-            'user_id' => $user->user_id,
-            'price' => 1000,
-            'paid_cash' => 1000,
-            'datetime' => new DateTime('2021-12-30 12:00:00'),
-        ]);
-
+        # create accounting records
         $individual_accounting_list = \App\Models\IndividualAccountingList::create(
             [
                 'name' => '2020年度引き継ぎ',
@@ -63,12 +51,57 @@ class DatabaseSeeder extends Seeder
             ]
         );
 
-        \App\Models\IndividualAccountingRecord::create([
-            'user_id' => $user->user_id,
-            'price' => 1000,
-            'list_id' => $individual_accounting_list->list_id,
+        $users = \App\Models\User::all();
+        foreach ($users as $user) {
+            \App\Models\IndividualAccountingRecord::create([
+                'user_id' => $user->user_id,
+                'price' => random_int(1000, 10000),
+                'list_id' => $individual_accounting_list->list_id,
+            ]);
+        }
+
+        $accounting_list = \App\Models\AccountingList::create([
+            'name' => 'テスト集金',
+            'deadline' => date('Y-m-d', random_int(time() - (30 * 24 * 60 * 60), time() + (30 * 24 * 60 * 60))),
+            'admin' => AccountingType::GENERAL,
         ]);
 
+        foreach ($users as $user) {
+            if (random_int(0, 5) % 5 == 0) {
+                continue;
+            }
+            $price = random_int(1000, 10000);
+            $is_paid = random_int(0, 5) % 5 != 0;
+            if (! $is_paid) {
+                \App\Models\AccountingRecord::create([
+                    'accounting_id' => $accounting_list->accounting_id,
+                    'user_id' => $user->user_id,
+                    'price' => $price,
+                ]);
+                continue;
+            }
+
+            $paid_individual = min($price, $user->individualAccountingRecords->sum('price'));
+            $paid_cash = $price - $paid_individual;
+            $datetime = date('Y-m-d H:i:s', random_int(time() - (30 * 24 * 60 * 60), time()));
+            \App\Models\AccountingRecord::create([
+                'accounting_id' => $accounting_list->accounting_id,
+                'user_id' => $user->user_id,
+                'price' => $price,
+                'paid_cash' => $paid_cash,
+                'datetime' => $datetime,
+            ]);
+            if ($paid_individual == 0) {
+                continue;
+            }
+            \App\Models\IndividualAccountingRecord::create([
+                'user_id' => $user->user_id,
+                'price' => $paid_individual,
+                'accounting_id' => $accounting_list->accounting_id,
+            ]);
+        }
+
+        # create bulletin board
         $bulletin_board = \App\Models\BulletinBoard::create([
             'user_id' => $user->user_id,
             'title' => 'test',
