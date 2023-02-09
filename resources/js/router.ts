6e8dc6a8307db37d029c347a5dee106a09d769de
Nozomu Yamazaki <46639706+nozomu-y@users.noreply.github.com>
2023-02-10 01:14:41 +0900

    Update: refuse authentication if token is expired

--- resources/js/router.ts
+++ resources/js/router.ts
@@ -10,16 +10,15 @@ const routes = [
         path: "/",
         name: "home",
         component: Home,
-        meta: { requiresAuth: true },
     },
-    { path: "/login", name: "login", component: Login },
+    { path: "/login", name: "login", component: Login, meta: { guest: true } },
     {
         path: "/logout",
         name: "logout",
         component: {
             beforeRouteEnter(to, from, next) {
                 AuthApiService.logout()
-                    .then((response: LogoutResponse) => {
+                    .then((_response: LogoutResponse) => {
                         localStorage.removeItem("jwt");
                         next("/login");
                     })
@@ -40,8 +39,25 @@ router.beforeEach((to) => {
     if (to.name === "login" && localStorage.getItem("jwt") !== null) {
         return { name: "home" };
     }
-    if (to.meta.requiresAuth && localStorage.getItem("jwt") == null) {
-        return { name: "login" };
+    if (!to.meta.guest) {
+        const token = localStorage.getItem("jwt");
+        if (!token) {
+            return { name: "login" };
+        }
+        const base64Url = token.split(".")[1];
+        const base64 = base64Url.replace(/-/g, "+").replace(/_/g, "/");
+        const decodedToken = JSON.parse(
+            decodeURIComponent(escape(window.atob(base64)))
+        );
+        const expireDate = new Date(decodedToken.exp * 1000);
+        const now = new Date();
+        const isValidToken = now < expireDate;
+        if (isValidToken) {
+            return;
+        } else {
+            localStorage.removeItem("jwt");
+            return { name: "login" };
+        }
     }
 });
 
