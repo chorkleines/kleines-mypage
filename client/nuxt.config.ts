6cf8b46e175e7f7e9507535666be54ecba9103d6
Nozomu Yamazaki <46639706+nozomu-y@users.noreply.github.com>
2023-07-16 13:12:36 +0900

    feat: add vue-good-table-next

--- client/nuxt.config.ts
+++ client/nuxt.config.ts
@@ -1,12 +1,15 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-    runtimeConfig: {
-        public: {
-            baseURL: "http://localhost:8000",
-        },
+  runtimeConfig: {
+    public: {
+      baseURL: "http://localhost:8000",
     },
-    ssr: false,
-    modules: ["@nuxtjs/tailwindcss"],
-    css: ["@fortawesome/fontawesome-svg-core/styles.css"],
-    plugins: ["@/plugins/fontawesome.ts"],
+  },
+  ssr: false,
+  modules: ["@nuxtjs/tailwindcss"],
+  css: [
+    "@fortawesome/fontawesome-svg-core/styles.css",
+    "@/assets/styles/vue-good-table.scss",
+  ],
+  plugins: ["@/plugins/fontawesome.ts"],
 });
