08aecc037176fdda1134e09dcee77b17e57f8346
Nozomu Yamazaki <46639706+nozomu-y@users.noreply.github.com>
2023-07-16 13:13:06 +0900

    feat: add users view

---
+++ client/composables/useUsers.ts
@@ -0,0 +1,86 @@
+export type User = {
+  userId: number;
+  firstName: string;
+  lastName: string;
+  nameKana?: string;
+  grade: number;
+  part: string;
+  status: string;
+  fullName?: string;
+  statusFormatted?: string;
+  partFormatted?: string;
+};
+
+const createUser = (u) => {
+  let user: User | null = null;
+  if (u == null) {
+    user = null;
+  } else {
+    user = {
+      userId: u.user_id,
+      firstName: u.first_name,
+      lastName: u.last_name,
+      nameKana: u.name_kana,
+      grade: u.grade,
+      part: u.part,
+      status: u.status,
+    };
+  }
+  return user;
+};
+
+export const useUsers = () => {
+  const { getJWT } = useAuth();
+  const users = ref([]);
+
+  async function getUsers() {
+    const { data } = await useFetch("/api/users", {
+      baseURL: "http://localhost:8000",
+      method: "GET",
+      headers: {
+        Authorization: `Bearer ${getJWT()}`,
+      },
+    });
+    data.value.forEach((u) => {
+      const user = createUser(u);
+      users.value.push(user);
+    });
+    users.value.forEach((user: User) => {
+      user.fullName = `${user.lastName} ${user.firstName}`;
+      switch (user.status) {
+        case "PRESENT":
+          user.statusFormatted = "在団";
+          break;
+        case "ABSENT":
+          user.statusFormatted = "休団";
+          break;
+        default:
+          user.statusFormatted = "";
+          break;
+      }
+      switch (user.part) {
+        case "S":
+          user.partFormatted = "Soprano";
+          break;
+        case "A":
+          user.partFormatted = "Alto";
+          break;
+        case "T":
+          user.partFormatted = "Tenor";
+          break;
+        case "B":
+          user.partFormatted = "Bass";
+          break;
+        default:
+          user.partFormatted = "";
+          break;
+      }
+      users.value.push(user);
+    });
+  }
+
+  return {
+    users,
+    getUsers,
+  };
+};
--- client/pages/users.vue
+++ client/pages/users.vue
@@ -1,7 +1,80 @@
-<template></template>
+<template>
+  <div class="prose">
+    <div class="text-xs breadcrumbs pt-0">
+      <ul class="m-0 p-0">
+        <li>
+          <NuxtLink to="/">ホーム</NuxtLink>
+        </li>
+        <li>団員リスト</li>
+      </ul>
+    </div>
+    <h1 class="text-2xl">団員リスト</h1>
+    <div class="overflow-x-auto w-full not-prose max-w-lg">
+      <vue-good-table
+        :columns="columns"
+        :rows="users"
+        :sort-options="{
+          enabled: true,
+          initialSortBy: [
+            { field: 'grade', type: 'desc' },
+            { field: 'partFormatted', type: 'asc' },
+          ],
+        }"
+        :pagination-options="{
+          enabled: true,
+          perPage: 20,
+        }"
+        styleClass="table whitespace-nowrap w-full mb-3"
+      />
+    </div>
+  </div>
+</template>
 
 <script setup lang="ts">
 definePageMeta({
   middleware: "auth",
 });
+import { VueGoodTable } from "vue-good-table-next";
+const { users, getUsers } = useUsers();
+
+await getUsers();
+
+const columns = [
+  {
+    label: "学年",
+    field: "grade",
+    width: "5rem",
+  },
+  {
+    label: "パート",
+    field: "partFormatted",
+    sortFn: sortPart,
+    width: "7rem",
+  },
+  {
+    label: "氏名",
+    field: "fullName",
+    sortable: false,
+  },
+  {
+    label: "在団 / 休団",
+    field: "statusFormatted",
+    sortFn: sortStatus,
+    width: "7rem",
+  },
+];
+
+function sortPart(x: string, y: string, _col, _rowX, _rowY) {
+  const partOrder = ["Soprano", "Alto", "Tenor", "Bass"];
+  const xIndex = partOrder.indexOf(x);
+  const yIndex = partOrder.indexOf(y);
+  return xIndex < yIndex ? -1 : xIndex > yIndex ? 1 : 0;
+}
+
+function sortStatus(x: string, y: string, _col, _rowX, _rowY) {
+  const statusOrder = ["在団", "休団"];
+  const xIndex = statusOrder.indexOf(x);
+  const yIndex = statusOrder.indexOf(y);
+  return xIndex < yIndex ? -1 : xIndex > yIndex ? 1 : 0;
+}
 </script>
