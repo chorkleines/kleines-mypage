de8b2d0df0316b9b2a634305ac433d40bebd525f
Nozomu Yamazaki <46639706+nozomu-y@users.noreply.github.com>
2023-07-23 20:17:35 +0900

    test: add tests for authenticated users

--- api/tests/Feature/AuthTest.php
+++ api/tests/Feature/AuthTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature;
 
+use App\Enums\UserStatus;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
@@ -16,11 +17,38 @@ class AuthTest extends TestCase
         $response->assertStatus(401);
     }
 
+    public function test_get_user_info_using_authenticated_user()
+    {
+        $this->postJson('/login', [
+            'email' => 'admin@chorkleines.com',
+            'password' => 'password',
+        ]);
+        $response = $this->get('/api/me');
+
+        $response->assertStatus(200);
+        $response->assertJson([
+            'email' => 'admin@chorkleines.com',
+            'status' => UserStatus::PRESENT,
+        ]);
+    }
+
     public function test_check_if_authenticated_using_unauthenticated_user()
     {
         $response = $this->get('/api/auth');
 
         $response->assertStatus(200);
         $this->assertSame('unauthenticated', $response->json());
+    }
+
+    public function test_check_if_authenticated_using_authenticated_user()
+    {
+        $this->postJson('/login', [
+            'email' => 'admin@chorkleines.com',
+            'password' => 'password',
+        ]);
+        $response = $this->get('/api/auth');
+
+        $response->assertStatus(200);
+        $this->assertSame('authenticated', $response->json());
     }
 }
