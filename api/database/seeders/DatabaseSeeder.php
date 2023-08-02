89ee262b384d4a4d359cc1b7cff026f8172f0b55
Nozomu Yamazaki <46639706+nozomu-y@users.noreply.github.com>
2023-08-02 13:49:10 +0900

    feat: create users with status

--- api/database/seeders/DatabaseSeeder.php
+++ api/database/seeders/DatabaseSeeder.php
@@ -27,6 +27,18 @@ class DatabaseSeeder extends Seeder
         ]);
     }
 
+    public function createUser($status)
+    {
+        $user = \App\Models\User::factory()->create([
+            'email' => 'user_'.strtolower($status).'@chorkleines.com',
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password is password
+            'status' => $status,
+        ]);
+        \App\Models\Profile::factory()->create([
+            'user_id' => $user->id,
+        ]);
+    }
+
     /**
      * Seed the application's database.
      *
@@ -60,6 +72,10 @@ class DatabaseSeeder extends Seeder
         $this->createAdminUser(Role::MANAGER);
         $this->createAdminUser(Role::ACCOUNTANT);
         $this->createAdminUser(Role::CAMP);
+
+        $this->createUser(UserStatus::PRESENT);
+        $this->createUser(UserStatus::ABSENT);
+        $this->createUser(UserStatus::RESIGNED);
 
         // create random users
         \App\Models\User::factory(100)->has(\App\Models\Profile::factory())->create();
