a8506dbfd5b843fc55404c6dd8c30cc6e7513e84
Nozomu Yamazaki <46639706+nozomu-y@users.noreply.github.com>
2023-08-04 23:13:17 +0900

    refactor: remove unused getDescription

--- api/app/Enums/Part.php
+++ api/app/Enums/Part.php
@@ -15,40 +15,4 @@ final class Part extends Enum
     const TENOR = 'T';
 
     const BASS = 'B';
-
-    public static function getDescriptionAbbrev($value): string
-    {
-        if ($value == self::SOPRANO) {
-            return 'Sop.';
-        }
-        if ($value == self::ALTO) {
-            return 'Alt.';
-        }
-        if ($value == self::TENOR) {
-            return 'Ten.';
-        }
-        if ($value == self::BASS) {
-            return 'Bas.';
-        }
-
-        return parent::getDescription($value);
-    }
-
-    public static function getDescriptionJp($value): string
-    {
-        if ($value == self::SOPRANO) {
-            return 'ソプラノ';
-        }
-        if ($value == self::ALTO) {
-            return 'アルト';
-        }
-        if ($value == self::TENOR) {
-            return 'テナー';
-        }
-        if ($value == self::BASS) {
-            return 'ベース';
-        }
-
-        return parent::getDescription($value);
-    }
 }
--- api/app/Enums/PaymentMethod.php
+++ api/app/Enums/PaymentMethod.php
@@ -11,16 +11,4 @@ final class PaymentMethod extends Enum
     const CASH = 'CASH';
 
     const INDIVIDUAL_ACCOUNTING = 'INDIVIDUAL_ACCOUNTING';
-
-    public static function getDescription(mixed $value): string
-    {
-        if ($value == self::CASH) {
-            return '現金';
-        }
-        if ($value == self::INDIVIDUAL_ACCOUNTING) {
-            return '個別会計';
-        }
-
-        return parent::getDescription($value);
-    }
 }
