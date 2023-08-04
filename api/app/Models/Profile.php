be8563046204224979cc38bcd74acbfc389ef92b
Nozomu Yamazaki <46639706+nozomu-y@users.noreply.github.com>
2023-08-04 23:00:02 +0900

    refactor: remove column-sortable

--- api/app/Models/Profile.php
+++ api/app/Models/Profile.php
@@ -6,11 +6,10 @@ use App\Enums\Part;
 use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Kyslik\ColumnSortable\Sortable;
 
 class Profile extends Model
 {
-    use HasFactory, Sortable;
+    use HasFactory;
 
     protected $fillable = [
         'user_id',
@@ -40,37 +39,6 @@ class Profile extends Model
         'birthday',
     ];
 
-    public $sortable = ['name_kana', 'grade', 'part'];
-
-    private $partOrderRaw = 'CASE WHEN profiles.part LIKE "S" THEN 1 WHEN profiles.part LIKE "A" THEN 2 WHEN profiles.part LIKE "T" THEN 3 WHEN profiles.part LIKE "B" THEN 4 END';
-
-    public function nameSortable($query, $direction)
-    {
-        return $query
-            ->orderBy('name_kana', $direction)
-            ->orderBy('grade', 'ASC')
-            ->orderByRaw($this->partOrderRaw.' ASC')
-            ->select('profiles.*');
-    }
-
-    public function gradeSortable($query, $direction)
-    {
-        return $query
-            ->orderBy('grade', $direction)
-            ->orderByRaw($this->partOrderRaw.' ASC')
-            ->orderBy('name_kana', 'ASC')
-            ->select('profiles.*');
-    }
-
-    public function partSortable($query, $direction)
-    {
-        return $query
-            ->orderByRaw($this->partOrderRaw.' '.$direction)
-            ->orderBy('grade', 'ASC')
-            ->orderBy('name_kana', 'ASC')
-            ->select('profiles.*');
-    }
-
     public function user()
     {
         return $this->hasOne(User::class, 'user_id');
@@ -79,10 +47,10 @@ class Profile extends Model
     public function display_name()
     {
         $display_name =
-            str_pad($this->grade, 2, 0, STR_PAD_LEFT).
-            $this->part->value.
-            ' '.
-            $this->last_name.
+            str_pad($this->grade, 2, 0, STR_PAD_LEFT) .
+            $this->part->value .
+            ' ' .
+            $this->last_name .
             $this->first_name;
 
         return $display_name;
