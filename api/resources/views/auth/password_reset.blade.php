1fb596f7cd1eadd2f5fcb44f8c767ecd231d2f1e
Nozomu Yamazaki <46639706+nozomu-y@users.noreply.github.com>
2023-08-06 02:15:26 +0900

    fix: use nuxt url

--- api/resources/views/auth/password_reset.blade.php
+++ api/resources/views/auth/password_reset.blade.php
@@ -4,7 +4,7 @@
 パスワード再設定のリクエストがありました。
 
 以下のリンクをクリックしてパスワードを再設定してください。
-<x-mail::button :url="config('app.url').'/reset-password/'.$token">
+<x-mail::button :url="env('NUXT_URL', 'http://localhost').'/reset-password/'.$token">
 パスワード再設定
 </x-mail::button>
 有効期限は{{ config('auth.passwords.'.config('auth.defaults.passwords').'.expire') }}分間です。
