dfd4f0c5b388a0dd4ce7997fb3ca5fb64b64ebb7
Nozomu Yamazaki <46639706+nozomu-y@users.noreply.github.com>
2023-08-12 17:37:08 +0900

    feat: add api-doc for login and logout

--- api/app/Http/Controllers/Auth/LoginController.php
+++ api/app/Http/Controllers/Auth/LoginController.php
@@ -5,6 +5,43 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
 
+/**
+ * @OA\Post(
+ *     path="/login",
+ *     summary="Login the user",
+ *     tags={"Auth"},
+ *     @OA\RequestBody(
+ *         @OA\MediaType(
+ *             mediaType="application/json",
+ *             @OA\Schema(
+ *                 @OA\Property(
+ *                     property="email",
+ *                     type="string"
+ *                 ),
+ *                 @OA\Property(
+ *                     property="password",
+ *                     type="string"
+ *                 ),
+ *                 example={"email": "admin@chorkleines.com", "password": "password"}
+ *             )
+ *         )
+ *     ),
+ *     @OA\Response(
+ *         response=204,
+ *         description="Success",
+ *     ),
+ * )
+ *
+ * @OA\Post(
+ *     path="/logout",
+ *     summary="Logout the user",
+ *     tags={"Auth"},
+ *     @OA\Response(
+ *         response=204,
+ *         description="Success",
+ *     ),
+ * )
+ */
 class LoginController extends Controller
 {
     /*
