d5c8da7444cd40b8b9581df05c60cdc9d6b2e45f
Nozomu Yamazaki <46639706+nozomu-y@users.noreply.github.com>
2023-08-12 22:12:13 +0900

    feat: create a custom error page

---
+++ client/error.vue
@@ -0,0 +1,21 @@
+<template>
+  <NuxtLayout>
+    <div
+      class="flex justify-center items-center h-full flex-col gap-5 prose mx-auto"
+    >
+      <div class="text-6xl font-bold">{{ error?.statusCode }}</div>
+      <div class="">{{ error?.message }}</div>
+      <div class="not-prose">
+        <a class="btn" href="/"> ホームに戻る </a>
+      </div>
+    </div>
+  </NuxtLayout>
+</template>
+
+<script setup lang="ts">
+const error = useError();
+definePageMeta({
+  layout: "guest",
+  middleware: "auth",
+});
+</script>
